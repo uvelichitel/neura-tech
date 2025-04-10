ce2542ff53ff18107dbf048348232a15d5e9ab20
uvelichitel <uvelichitel@gmail.com>
2025-04-11 00:32:35 +0300

    Merge complex branch

--- acquiring/main.go
+++ acquiring/main.go
@@ -33,6 +33,8 @@ const (
 	REFUNDED
 )
 
+const layout = "060102150405"
+
 var (
 	notificationURL  = os.Getenv("NOTIFICATION_URL")
 	hmacSecret       = []byte(os.Getenv("HMACSECRET")) // TODO
@@ -40,11 +42,18 @@ var (
 	terminalKey      = os.Getenv("TERMINAL_KEY")
 	connStr          = os.Getenv("CONN_STR")
 	signalURL        = os.Getenv("SIGNAL_URL")
-	db               *sql.DB
-	stmts            = make(map[string]*sql.Stmt)
+
+// db               *sql.DB
+// stmts            = make(map[string]*sql.Stmt)
 )
 var logWriter io.Writer
 var logger *log.Logger = log.New(logWriter, "neura", log.LstdFlags)
+var db Store
+
+type Store struct {
+	stmts map[string]*sql.Stmt
+	*sql.DB
+}
 
 // Tbank
 type InitOrder struct {
@@ -52,40 +61,26 @@ type InitOrder struct {
 	Description string `json:"Description"`
 }
 
-type Order struct {
-	InitOrder
-	OrderId string `json:"OrderId"`
-	//	Amount      int64     `json:"Amount"`
-	//	Description string    `json:"Description"`
-	CustomerKey string    `json:"CustomerKey"` //UserID
-	Date        time.Time // TODO
-}
-
 type DATA struct {
 	OperationInitiatorType string `json:"OperationInitiatorType,omitempty"`
 	CustomerID             string `json:"CustomerID,omitempty"`
 }
 
-type Payment struct {
-	PaymentId string `json:"PaymentId,omitempty"`
-	Order
-	Status    string `json:"Status"`
-	Success   bool   `json:"Success"`
-	Recurrent string `json:"Recurrent,omitempty"`
-	PayType   string `json:"PayType,omitempty"`
-	DATA      `json:"DATA"`
-	Date      time.Time
-}
-
 type InitPayment struct {
-	Payment
 	TerminalKey     string `json:"TerminalKey,omitempty"`
+	Amount          int64  `json:"Amount,omitempty"`
+	OrderId         string `json:"OrderId,omitempty"`
 	Token           string `json:"Token,omitempty"`
+	Description     string `json:"Description,omitempty"`
+	CustomerKey     string `json:"CustomerKey,omitempty"`
+	Recurrent       string `json:"Recurrent,omitempty"`
+	PayType         string `json:"PayType,omitempty"`
 	Language        string `json:"Language,omitempty"`
 	NotificationURL string `json:"NotificationURL,omitempty"`
 	SuccessURL      string `json:"SuccessURL,omitempty"`
 	FailURL         string `json:"FailURL,omitempty"`
 	RedirectDueDate string `json:"RedirectDueDate,omitempty"`
+	DATA            `json:"DATA,omitempty"`
 	//Receipt Receipt `json:"Receipt ,omitempty"`
 }
 
@@ -95,7 +90,7 @@ type InitResponse struct {
 	OrderId     string `json:"OrderId"`
 	Success     bool   `json:"Success"`
 	Status      string `json:"Status"`
-	PaymentId   string `json:"PaymentId"`
+	PaymentId   string `json:"PrsaymentId"`
 	ErrorCode   string `json:"ErrorCode"`
 	PaymentURL  string `json:"PaymentURL"`
 	Message     string `json:"Message"`
@@ -144,16 +139,11 @@ type CanselResponse struct { // TODO
 }
 
 type PaymentSignal struct {
-	Payment_id string `json:"payment_id"`
-	User_id    int64  `json:"user_id"`
-	Amount     int64  `json:"amount"`
-	Status     string `json:"status"`
-	Signature  string `json:"signature"`
-}
-
-type sqlDB struct {
-	stmts map[string]*sql.Stmt
-	*sql.DB
+	Payment_id string  `json:"payment_id"`
+	User_id    int64   `json:"user_id"`
+	Amount     float64 `json:"amount"`
+	Status     string  `json:"status"`
+	Signature  string  `json:"signature"`
 }
 
 //func ConnectionString() string {
@@ -165,23 +155,19 @@ type sqlDB struct {
 //	return connStr
 //}
 
-func Connect() *sql.DB {
+func Connect(connStr string) (*sql.DB, error) {
 	//connStr := ConnectionString()
 
 	db, err := sql.Open("pgx", connStr)
 
 	if err != nil {
-		log.Fatalf("Unable to connect to database because %s", err)
+		return nil, err
 	}
-
 	if err = db.Ping(); err != nil {
-
-		log.Fatalf("Cannot ping database because %s", err)
-
+		return nil, err
 	}
-
 	log.Println("Successfully connected to database and pinged it")
-	return db
+	return db, nil
 }
 
 func (i *InitPayment) CalcToken() {
@@ -202,20 +188,10 @@ func IsValidToken(tokenstring string) (string, error) { // TODO if to validate
 		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 		return hmacSecret, nil
 	})
-	//if err != nil {
-	//	return "", err
-	//}
-	//if !token.Valid {
-	//	return "", errors.New("Плохой ключ. Попробуйте авторизоваться заново.")
-	//}
 	claims, ok := token.Claims.(jwt.MapClaims)
 	if !ok {
 		return "", errors.New("Поддельный ключ. Попробуйте авторизоваться заново.")
 	}
-	//end, err := claims.GetExpirationTime()
-	//if (err != nil) || end.Time.Before(time.Now()) {
-	//	return "", errors.New("Ключ протух. Попробуйте авторизоваться заново.")
-	//}
 	customerKey, err := claims.GetSubject()
 	if err != nil {
 		return "", err
@@ -228,50 +204,133 @@ func GetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := json.NewDecoder(r.Body).Decode(notification); err != nil {
 		//TODO
 	}
+	userID, err  := strconv.ParseInt(notification.OrderId[:13], 10, 64)
+	if err != nil {
+		// TODO
+	}
+	p := new(PaymentSignal)
+	p.Amount = float64(notification.Amount) / 100
+	p.Status = notification.Status
+	p.Payment_id = notification.PaymentId.String()
+	p.User_id = userID 
+	if err != nil {
+		// TODO
+	}
+	p.Sign()
+	err = Signal(p)
+	if err != nil {
+		// TODO recall
+	}
+	err = db.UpdateStatus(notification)
+	if err != nil {
+		// TODO
+	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
-	_, err := stmts["notification"].Exec(notification.Status, notification.PaymentId)
-	if err != nil {
-		// TODO
-	}
-	if notification.Status != "CONFIRMED" {
-		//TODO
-	}
 }
 
-func SignSignal(secret []byte, data string) string {
+func (s *PaymentSignal) Sign() {
 	// Create a new HMAC by defining the hash type and the key (as byte array)
-	h := hmac.New(sha256.New, secret)
+	h := hmac.New(sha256.New, hmacSecret)
 	// Write Data to it
+	amount := strconv.FormatFloat(s.Amount, 'f', 2, 64)
+	userID := strconv.FormatInt(s.User_id, 10)
+	data := s.Payment_id + userID + amount +s.Status
 	h.Write([]byte(data))
 	// Get result and encode as hexadecimal string
-	sha := hex.EncodeToString(h.Sum(nil))
-	return sha
+	s.Signature =  hex.EncodeToString(h.Sum(nil))
 }
 
-func (p *Payment) Signal() {
-	user_id, err := strconv.ParseInt(p.CustomerKey, 10, 64)
-	if err != nil {
-		// TODO
-	}
-	data := p.PaymentId + p.CustomerKey + strconv.FormatInt(p.Amount, 10) + p.Status // TODO p.Amount (100.0)
-	signal := PaymentSignal{
-		Payment_id: p.PaymentId,
-		User_id:    user_id,
-		Amount:     p.Amount,
-		Status:     p.Status,
-		Signature:  SignSignal(hmacSecret, data),
-	}
-	json, err := json.Marshal(signal)
+func Signal(s *PaymentSignal) error {
+	json, err := json.Marshal(s)
 	if err != nil {
 		// TODO
 	}
 	body := bytes.NewBuffer(json)
 	resp, err := http.Post(signalURL, "application/json", body)
 	if err != nil {
-		// TODO
+		return err
+	}
+	if resp.StatusCode != 200 {
+		return errors.New("StatusCode " + strconv.FormatInt(int64(resp.StatusCode), 10))
 	}
 	defer resp.Body.Close()
+	return nil
+}
+
+func EncodeOrderId(u string) string {
+	t := time.Now().Format(layout)
+	return u + "#" + t
+}
+
+func DecodeOrderId(n *Notification) (string, time.Time) {
+	u := n.OrderId[12:]
+	t, err := time.Parse(layout, n.OrderId[:13])
+	if err != nil {
+		t = time.Now()
+	}
+	return u, t
+}
+func (n *Notification) Persist(s Store) error {
+	customerKey, time := DecodeOrderId(n)
+	_, err := s.stmts["payment"].Exec(n.PaymentId, n.Status, time, n.OrderId, n.Amount, customerKey)
+	return err
+}
+
+func (s Store) UpdateStatus(n *Notification) error {
+	_, err := s.stmts["updateStatus"].Exec(n.Status, n.PaymentId)
+	return err
+}
+
+func (s Store) Remove(n Notification) error {
+	_, err := s.stmts["remove"].Exec(n.PaymentId)
+	return err
+}
+
+func InitiatePayment(o *InitOrder, u string) (string, error) {
+	o.Amount *= 100
+	orderId := EncodeOrderId(u)
+	req := &InitPayment{
+		TerminalKey: terminalKey,
+		Amount: o.Amount,
+		OrderId: orderId,
+		Description: o.Description,
+		CustomerKey: u,
+		Language:        "ru",
+		NotificationURL: notificationURL,
+		//		SuccessURL:      SuccessURL,
+		//		FailURL:         FailURL,
+		//DATA: Data{
+		//	OperationInitiatorType: "0",
+		//},
+	}
+	req.CalcToken()
+	jsn, err := json.Marshal(req)
+	if err != nil {
+		//	w.WriteHeader(http.StatusBadRequest)
+		//	w.Write([]byte(err.Error()))
+		return "", err
+	}
+	cont := bytes.NewReader(jsn)
+	resp, err := http.Post("https://securepay.tinkoff.ru/v2/Init", "application/json", cont)
+	// resp, err := http.Post("https://rest-api-test.tinkoff.ru/v2/init", "application/json", bytes.NewReader(jsn))
+	if err != nil {
+		//		w.WriteHeader(http.StatusInternalServerError)
+		//		w.Write([]byte(err.Error()))
+		return "", err
+	}
+	if resp.StatusCode != 200 {
+		//	w.WriteHeader(resp.StatusCode)
+		//	w.Write([]byte("Платеж не проходит"))
+		return "", errors.New("Платеж не проходит")
+		//TODO
+	}
+	rspns := new(InitResponse)
+	err = json.NewDecoder(resp.Body).Decode(rspns)
+	if err != nil {
+		return "", err
+	}
+	return  rspns.PaymentURL, nil
 }
 
 func Pay(w http.ResponseWriter, r *http.Request) {
@@ -296,76 +355,67 @@ func Pay(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	initOrder.Amount *= 100
-	orderId := new(int64)
-	err = db.QueryRow("SELECT nextval('orderid')").Scan(orderId)
-	req := &InitPayment{
-		TerminalKey: terminalKey,
-		Payment: Payment{
-			Order: Order{
-				OrderId:     strconv.FormatInt(*orderId, 10),
-				InitOrder:   *initOrder,
-				CustomerKey: user,
-			},
-		},
-		Language:        "ru",
-		NotificationURL: notificationURL,
-		//		SuccessURL:      SuccessURL,
-		//		FailURL:         FailURL,
-		//DATA: Data{
-		//	OperationInitiatorType: "0",
-		//},
-	}
-	req.CalcToken()
-	jsn, err := json.Marshal(req)
+	//orderId := new(int64)
+	//err = db.QueryRow("SELECT nextval('orderid')").Scan(orderId)
+	//if err != nil {
+	// TODO
+	//}
+	//order := Order{
+	//	OrderId:     strconv.FormatInt(*orderId, 10),
+	//	InitOrder:   *initOrder,
+	//	CustomerKey: user,
+	//}
+	paymentURL, err := InitiatePayment(initOrder, user)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
 		return
 	}
-	cont := bytes.NewReader(jsn)
-	resp, err := http.Post("https://securepay.tinkoff.ru/v2/Init", "application/json", cont)
-	// resp, err := http.Post("https://rest-api-test.tinkoff.ru/v2/init", "application/json", bytes.NewReader(jsn))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-		//TODO
-	}
-	if resp.StatusCode != 200 {
-		w.WriteHeader(resp.StatusCode)
-		w.Write([]byte("Платеж не проходит"))
-		return
-		//TODO
-	}
-	rspns := new(InitResponse)
-	err = json.NewDecoder(resp.Body).Decode(rspns)
-	if err != nil {
-		panic(err)
-	}
-	_, err = stmts["payment"].Exec(rspns.PaymentId, rspns.Status, time.Now, rspns.OrderId, rspns.Amount, req.Description, req.CustomerKey)
-	if err != nil {
-		// TODO
-	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(rspns.PaymentURL))
+	w.Write([]byte(paymentURL))
+}
+
+func MakeStore() (db Store, err error) {
+	if db.DB, err = Connect(connStr); err != nil {
+		return
+	}
+	db.stmts = make(map[string]*sql.Stmt)
+	return db, nil
+}
+
+func (s Store) Init() (err error) {
+	s.stmts["updateStatus"], err = s.Prepare("UPDATE payments SET status = $1 WHERE paymentid = $2")
+	if err != nil {
+		return
+	}
+	db.stmts["payment"], err = s.Prepare("INSERT INTO payments (paymentid, status, date, orderid, amount, customerkey) VALUES ($1, $2, $3, $4, $5, $6)")
+	if err != nil {
+		return
+	}
+	db.stmts["remove"], err = s.Prepare("DELETE FROM payments WHERE paymentid = $1")
+	if err != nil {
+		return
+	}
+	return nil
+}
+
+func (s Store) Cleanup() {
+	for _, v := range s.stmts {
+		v.Close()
+	}
+	s.Close()
 }
 
 func main() {
+
 	l, err := net.Listen("tcp", ":5000")
 	if err != nil {
-		print(err.Error)
+		log.Fatal(err)
 	}
-	db := Connect()
-	stmts["notification"], err = db.Prepare("UPDATE payments SET status = $1 WHERE paymentid = $2")
+	db, err = MakeStore()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer stmts["notification"].Close()
-	stmts["payment"], err = db.Prepare("INSERT INTO payments (paymentid, status, date, orderid, amount, description, customerkey) VALUES ($1, $2, $3, $4, $5, $6, $7)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmts["payment"].Close()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/payment", Pay)
 	mux.HandleFunc("/notification", GetNotification)
